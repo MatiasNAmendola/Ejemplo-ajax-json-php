fe8c743d85deb53f0178fe6ab503b17609fdc87e
Matias Nahuel Améndola <soporte.esolutions@gmail.com>
2013-11-03 08:42:02 -0300

    ajax-json-php

---
+++ cliente.js
@@ -0,0 +1,57 @@
+﻿function contacto(){
+e.preventDefault();
+
+// recogemos los datos en el objeto info
+
+var info = {
+
+"nombre" : $("input[name='nombre']").val(),
+"correo" : $("input[name='correo']").val().replace("@","\@"),
+"empresa" : $("input[name='empresa']").val(),
+"mensaje" : $("textarea[name='mensaje']").val()
+
+};
+
+json_info = JSON.stringify(info);  // convertimos el objeto a una cadena JSON
+
+// a través de este método le enviamos el JSON json_info al backend
+$.getJSON(
+
+"servidor.php", //url
+{info : json_info }, //aquí como podemos ver es donde pasamos el json que contiene todas las variables
+function(json){
+
+//aquí es donde recibimos la respuesta del servidor, mostraremoos un mensaje dependiendo el resultado de la petición (fallida/exitosa)
+
+if(json.resp == 1) /
+alert("Hemos recibido tu mensaje en breve lo responderemos");
+else if (json.resp == 0)
+alert("Parece que no has llenado bien los campos, Verificalos e intenta nuevamente.");
+
+else if(json.resp == 3)
+
+alert("Nuestro sistema no puede enviar el mensaje en esye momento, por favor intenta luego");
+
+}
+
+);
+
+}
+
+function ini_app(){
+
+//con esto controlamos y modificamos la acción de envío por defecto del formulario
+
+$("form[name='contacto']").submit(function(e){
+
+contacto(e);  // como estamos controlando lo que sucede al enviar el formulario, entonces llamamos a la función contacto que se encarga de recoger y enviar al backend los datos recogidos
+
+});
+
+}
+
+$(document).ready(function(){
+
+ini_app(); //iniciamos nuestra pequeña app
+
+});
---
+++ servidor.php
@@ -0,0 +1,40 @@
+﻿function env_correo(){
+$data = json_decode(stripslashes($_GET["info"])); // recogemos el JSON enviado desde JS y eliminamos los slashes /
+
+//verificamos que las variables sean válidas
+
+if( !empty($data->nombre) // validamos que el nombre no este vacío
+and filter_var($data->correo,FILTER_VALIDATE_EMAIL) // validamos que el correo, sea un correo válido
+and !empty($data->mensaje) // validamos que el mensaje no este vacío
+
+){
+$headers = 'From: '. $data->correo . "\r\n" . // correo que remite
+'Reply-To: '. $data->correo . "\r\n" . // correo al que responde el mensaje quien lo reciba
+'X-Mailer: PHP/' . phpversion(); // esto es importante para que no nos marquen como spam
+
+$data->mensaje = trim($data->mensaje); // eliminamos los espacios en blanco del mensaje para ahorrar caracteres
+
+$firma = "\r\n\r\n\r\n\r\nSaludos Cordiales,"; // firma del correo
+
+if (
+
+mail("info@gomosoft.com", //correo al que se enviará
+"contacto desde la web", $data->msg . $firma, // contenido del correo
+$headers // cabeceras del mensaje para que no sea marcado como spam
+)
+
+)
+
+echo json_encode(array("resp" => 1)); // regresamos un 1 si tenemos éxito enviando el mensaje
+
+else
+
+echo json_encode(array("resp" => 3)); // regresamos un 3 si el envío falla en el servidor
+}
+
+else
+
+echo json_encode(array("resp" => 0)); // enviamos 0 si las variables no pasán los filtros
+}
+
+env_correo(); // llamamos a la función que se encarga de el envío del mensaje
